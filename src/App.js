fa056916e5512d833f1aaddeba05309872270dc7
Shepe1304 <hqgiang1346@gmail.com>
2024-11-17 16:34:13 -0500

    initialized login page and added favicon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
 import Background from "./components/layout/background/Background";
 import Header from "./components/layout/header/Header";
 import Homepage from "./components/pages/homepage/Homepage";
+import Login from "./components/pages/login/Login";
 
 function App() {
   return (
     <div className="App">
       <Background />
       <Header />
-      <Homepage />
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./Header.css";
 import logo from "../../assets/img/Logo.png";
+import { useNavigate } from "react-router";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const HandleLoginClick = () => {
+    navigate("/login");
+  };
+
+  const HandleHomeClick = () => {
+    navigate("/");
+  };
+
   return (
     <header className="header">
-      <div className="header--logo">
+      <div className="header--logo" onClick={HandleHomeClick}>
         <img
           src={logo}
           alt="Logo, a smiling globe with university hat and bull horns, and the text 'MentorUni' around it."
@@ -15,12 +26,16 @@ const Header = () => {
         <span>Find Mentor</span>
       </div>
       <div className="header--options">
-        <div className="header--option">Home</div>
+        <div className="header--option" onClick={HandleHomeClick}>
+          Home
+        </div>
         <div className="header--option">About</div>
         <div className="header--option">Contact</div>
         <div className="header--option">FAQ</div>
       </div>
-      <div className="header--user_login">Log in</div>
+      <div className="header--user_login" onClick={HandleLoginClick}>
+        Log in
+      </div>
     </header>
   );
 };
