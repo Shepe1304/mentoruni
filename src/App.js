43d439da04f3741b28dca5ce5c939cfc74bb06a2
Daho Mita <dahomita17062006@gmail.com>
2024-11-18 15:28:01 -0500

    FAQs desgin

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import Background from "./components/layout/background/Background";
 import Header from "./components/layout/header/Header";
 import Homepage from "./components/pages/homepage/Homepage";
 import Login from "./components/pages/login/Login";
+import FAQs from "./components/pages/faqs/FAQs";
 
 function App() {
   return (
     <div className="App">
+      {/* <Background />
+      <Header />
+      <FAQs /> */}
+      
       <Background />
       <Header />
+    
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route exact path="/login" element={<Login />} />
