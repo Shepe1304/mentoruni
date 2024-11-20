fbefd6f9805791e60eaeb5ee990fac707a0503b0
Shepe1304 <hqgiang1346@gmail.com>
2024-11-20 02:34:03 -0500

    Styled Find Mentors page with Responsivity and displayed example mentors

--- src/components/layout/mentor-card/MentorCard.js
+++ src/components/layout/mentor-card/MentorCard.js
@@ -1,27 +1,54 @@
 import React from "react";
 import "./MentorCard.css";
+import star from "../../assets/img/star.png";
 
-const MentorCard = () => {
+const MentorCard = (props) => {
   return (
     <div className="mentor_card">
       <div className="mentor_card--profile_picture">
-        <img src="" alt="" />
+        <img src={props.img} alt="" />
       </div>
       <div className="mentor_card--info">
-        <div className="mentor_card--info_item" id="mentor_card--info_name">
-          Name: An Dinh
-        </div>
-        <div className="mentor_card--info_item" id="mentor_card--info_country">
-          Country: Vietnam
+        <div className="mentor_card--info_first_line">
+          <div className="mentor_card--info_item" id="mentor_card--info_name">
+            {props.name}
+          </div>
+          <div className="mentor_card--info_first_line_seperator">|</div>
+          <div
+            className="mentor_card--info_item"
+            id="mentor_card--info_country"
+          >
+            {props.country}
+          </div>
         </div>
         <div className="mentor_card--info_item" id="mentor_card--info_school">
-          School: University of South Earth
+          {props.school}
         </div>
-        <div className="mentor_card--info_item" id="mentor_card--info_gpa">
-          GPA: 10/10
+        <div className="mentor_card--info_item" id="mentor_card--info_skills">
+          Skills: {props.skills}
         </div>
-        <div className="mentor_card--info_item" id="mentor_card--info_rating">
-          <img src="" alt="rating star" />
+        <div className="mentor_card--info_last_line">
+          <div className="mentor_card--info_gpa_and_rating">
+            <div className="mentor_card--info_item" id="mentor_card--info_gpa">
+              GPA: {props.gpa}
+            </div>
+            <div
+              className="mentor_card--info_item"
+              id="mentor_card--info_rating"
+            >
+              {Array.from(Array(props.rating), (e, i) => {
+                return (
+                  <div className="mentor_card--info_rating--star_container">
+                    <img src={star} alt="Rating star" />
+                  </div>
+                );
+              })}
+              {/* <img src="" alt="rating star" /> */}
+            </div>
+          </div>
+          <div className="mentor_card--info_item" id="mentor_card--info_price">
+            ${props.price}/hour
+          </div>
         </div>
       </div>
     </div>
